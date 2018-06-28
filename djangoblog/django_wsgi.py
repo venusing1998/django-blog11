37a229944f8bb91bc0b3b56a0608270f22191738
venusing1998 <ago2chris@163.com>
2018-06-28 08:52:25 +0800

    update

--- djangoblog/django_wsgi.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#coding: utf-8
-
-import os
-import sys
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoblog.settings")#mysite替换为自己的项目�?
-from django.core.handlers.wsgi import WSGIHandler
-application = WSGIHandler()
