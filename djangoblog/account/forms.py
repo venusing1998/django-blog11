5ac906ac012943a51e544f6b22629c2fb932837d
chris <ago2chris@163.com>
2018-07-03 11:48:05 +0800

    update

--- djangoblog/account/forms.py
+++ djangoblog/account/forms.py
@@ -1,13 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Confirm Password", widget=forms.PasswordInput)
 
     class Meta:
         model = User
@@ -18,3 +22,9 @@ class RegistrationForm(forms.ModelForm):
         if cd["password"] != cd["password2"]:
             raise forms.ValidationError("passwords do not match.")
         return cd["password2"]
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ("phone",)
--- djangoblog/account/views.py
+++ djangoblog/account/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, UserProfileForm
 
 
 def user_login(request):
@@ -26,16 +26,37 @@ def user_login(request):
         return render(request, "account/login.html", {"form": login_form})
 
 
+# def register(request):
+#     if request.method == "POST":
+#         user_form = RegistrationForm(request.POST)
+#         if user_form.is_valid():
+#             new_user = user_form.save(commit=False)
+#             new_user.set_password(user_form.cleaned_data['password'])
+#             new_user.save()
+#             return HttpResponse("successfully")
+#         else:
+#             return HttpResponse("Sorry, your can not register.")
+#     else:
+#         user_form = RegistrationForm()
+#         return render(request, "account/register.html", {"form": user_form})
+
 def register(request):
     if request.method == "POST":
         user_form = RegistrationForm(request.POST)
-        if user_form.is_valid():
+        userprofile_form = UserProfileForm(request.POST)
+        if user_form.is_valid()*userprofile_form.is_valid():
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            return HttpResponse("successfully")
+            new_profile = userprofile_form.save(commit=False)
+            new_profile.user = new_user
+            new_profile.save()
+            # UserInfo.objects.create(user=new_user)
+            return HttpResponse("Successfully")
+            # return HttpResponseRedirect(reverse("account:user_login"))
         else:
             return HttpResponse("Sorry, your can not register.")
     else:
         user_form = RegistrationForm()
-        return render(request, "account/register.html", {"form": user_form})
+        userprofile_form = UserProfileForm()
+        return render(request, "account/register.html", {"form": user_form, "profile": userprofile_form})
